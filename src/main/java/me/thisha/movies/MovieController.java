2d8a0b83025fd0719432d01d4dcad94b09c6dc73
Dhanuja Thishakya <d.thishakya@gmail.com>
2023-12-03 23:27:11 +0530

    addreview api

--- src/main/java/me/thisha/movies/MovieController.java
+++ src/main/java/me/thisha/movies/MovieController.java
@@ -1,6 +1,5 @@
 package me.thisha.movies;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/v1/movies")
+@RequestMapping("/api/v1/movies")
 public class MovieController {
     @Autowired
     private MovieService movieService;
@@ -22,8 +21,8 @@ public class MovieController {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
     }
 
-    @GetMapping("/{imdbId}")
-    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
-        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId),HttpStatus.OK);
+    @GetMapping("/{ImdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String ImdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(ImdbId),HttpStatus.OK);
     }
 }
