dae19325e9663e1af43795955ad9b883374a584c
Dhanuja Thishakya <d.thishakya@gmail.com>
2023-12-06 08:42:54 +0530

    add post route

--- src/main/java/me/thisha/movies/ReviewController.java
+++ src/main/java/me/thisha/movies/ReviewController.java
@@ -16,7 +16,7 @@ public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
-    @PostMapping
+    @PostMapping("/post")
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
         return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
     }
