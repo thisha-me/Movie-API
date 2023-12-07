b05ab261dff306c68fca755c29452b50182ce5fa
Dhanuja Thishakya <d.thishakya@gmail.com>
2023-12-07 23:54:38 +0530

    remove movie mapping

--- src/main/java/me/thisha/movies/ReviewController.java
+++ src/main/java/me/thisha/movies/ReviewController.java
@@ -16,7 +16,7 @@ public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
-    @PostMapping("/post")
+    @PostMapping
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
         return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
     }
